36b670cd96a797a3eb3dbee2f1bd76e21ddad0c3
Erwin <34348385+ErwinRussel@users.noreply.github.com>
2024-02-10 11:57:38 +0000

    Update speech recognition

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,22 +1,62 @@
 'use client'
 
-import MicIcon from "@/components/MicIcon"
-import {useState, useRef} from 'react';
+import MicIcon from "@/components/micicon"
+import {useState, useEffect, useRef} from 'react';
+// import annyang from 'annyang';
 
 export default function Home() {
   const [transcription, setTranscription] = useState('');
   const [isListening, setIsListening] = useState(false);
   const recognitionRef = useRef(null);
   const timeoutRef = useRef(null);
+  
+  // useEffect(() => {
+  //   if (annyang) {
+  //     annyang.addCommands({
+  //       'Hey Vision': () => {
+  //         startListening();
+  //       }
+  //     });
+  //     annyang.start();
+  //     return () => {
+  //       annyang.removeCommands();
+  //       annyang.abort();
+  //     };
+  //   }
+
+  // }, []);
+
+  // const startListening = () => {
+  //   const listenSound = new Audio('/listen.mp3')
+  //   listenSound.play();
+  //   setIsListening(true);
+  //   annyang.addCallback('result', function(phrases) {
+  //   const successSound = new Audio('/success.mp3');
+  //     successSound.play();
+  //     setTranscription(phrases[0]);
+  //     stopListening();
+  //   });
+  // };
+
+  // const stopListening = () => {
+  //   setIsListening(false);
+  //   annyang.removeCallback('result');
+  // };
 
   const startListening = () => {
     setIsListening(true);
+    const listenSound = new Audio('/listen.mp3')
+    listenSound.play();
     const recognition = new window.webkitSpeechRecognition();
     recognitionRef.current = recognition;
+
     recognition.onstart = () => {
       console.log('Speech recognition started');
     };
+
     recognition.onresult = (event) => {
+      const successSound = new Audio('/success.mp3');
+      successSound.play();
       clearTimeout(timeoutRef.current);
       const transcript = event.results[0][0].transcript;
       setTranscription(transcript);
@@ -24,14 +64,19 @@ export default function Home() {
         stopListening();
       }, 2000); // Adjust this value as needed
     };
+
     recognition.onerror = (event) => {
       console.error('Speech recognition error:', event.error);
+      const errorSound = new Audio('/fail.mp3');
+      errorSound.play();
       stopListening();
     };
+
     recognition.onend = () => {
       setIsListening(false);
       console.log('Speech recognition ended');
     };
+
     recognition.start();
   };
 
@@ -54,8 +99,8 @@ export default function Home() {
       </header>
       <main className="flex-1 flex items-center justify-center w-full">
         <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6">
-          <button size="lg" variant="outline">
-            <MicIcon onClick={isListening ? stopListening : startListening} className="h-6 w-6" />
+          <button onClick={isListening ? stopListening : startListening} size="lg" className={`border rounded-lg p-4 transition ease-in-out delay-50 hover:bg-slate-500 ${isListening ? 'bg-red-600' : ''}`} variant="outline">
+            <MicIcon className="h-6 w-6" />
             Speak
           </button>
         </div>
--- frontend/src/components/micicon.js
+++ frontend/src/components/micicon.js
@@ -1,7 +1,6 @@
-export default function MicIcon(props) {
+export default function MicIcon() {
     return (
         <svg
-            {...props}
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
